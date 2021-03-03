44671fb7056c30505b18b10e1f8af0eb3be29627
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-03-03 15:51:21 +0500

    Lesson1ex3 refactoring

--- Assets/Asteroids/Code/Lesson1ex3.cs
+++ Assets/Asteroids/Code/Lesson1ex3.cs
@@ -2,33 +2,28 @@ class Program
 {
     static void Main(string[] args)
     {
+        const string QUIT = "q";
+        const string GREETING = "Здравствуйте, вас приветствует математическая программа\nпожалуйста введите число: ";
+        
         int factorial = 1;
         int sum = 0;
         int maxEven = 0;
 
-        Console.WriteLine("Здравствуйте, вас приветствует математическая программа\nпожалуйста введите число N.\n");
+        Console.WriteLine(GREETING);
 
         string input = Console.ReadLine();
 
-        if (input == "q")
-        {
-            return;
-        }
+        if (input.Equals(QUIT)) return;
 
         for (int i = 1; i <= Int32.Parse(input); i++)
         {
             factorial *= i;
             sum += i;
-
-            if (i%2 == 0)
-            {
-                maxEven = i;
-            }
+            if (i%2 == 0) maxEven = i;
         }
 
         Console.WriteLine($"Факториал равен {factorial}\n");
-        Console.WriteLine($"Сумма от 1 до N равна {sum}\n");
-        Console.WriteLine($"Максимальное четное число меньше N равно {maxEven}\n");
-        Console.ReadLine();
+        Console.WriteLine($"Сумма от 1 до {input} равна {sum}\n");
+        Console.WriteLine($"Максимальное четное число меньше {input} равно {maxEven}\n");
     }
 }
