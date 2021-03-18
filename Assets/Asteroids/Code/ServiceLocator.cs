4b54de07d8b960a821681c2a25e66e1366a18289
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-03-18 21:07:09 +0500

    Service locator realized

--- Assets/Asteroids/Code/ServiceLocator.cs
+++ Assets/Asteroids/Code/ServiceLocator.cs
@@ -1,1 +1,41 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
 
+
+namespace Asteroids
+{
+    internal static class ServiceLocator
+    {
+        #region Fields
+
+        private static readonly Dictionary<Type, object> _serviceContainer = new Dictionary<Type, object>();
+
+        #endregion
+
+
+        #region Methods
+
+        internal static void SetService<T>(T value) where T : class
+        {
+            var key = typeof(T);
+            if (!_serviceContainer.ContainsKey(key))
+            {
+                _serviceContainer[key] = value;
+            }
+        }
+
+        internal static T Resolve<T>()
+        {
+            var key = typeof(T);
+            if (_serviceContainer.ContainsKey(key))
+            {
+                return (T)_serviceContainer[key];
+            }
+
+            return default;
+        }
+
+        #endregion
+    }
+}
