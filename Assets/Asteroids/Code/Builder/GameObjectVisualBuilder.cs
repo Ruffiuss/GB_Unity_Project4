f76af3b59b9df0d807235518f0274086f8ebff38
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-03-20 21:15:34 +0500

    Builder for bullets realized

---
+++ Assets/Asteroids/Code/Builder/GameObjectVisualBuilder.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+
+namespace Asteroids
+{
+    internal sealed class GameObjectVisualBuilder : GameObjectBuilder
+    {
+        #region ClassLifeCycles
+
+        public GameObjectVisualBuilder(GameObject gameObject) : base(gameObject) { }
+
+        #endregion
+
+
+        #region Methods
+
+        public GameObjectVisualBuilder Name(string name)
+        {
+            _gameObject.name = name;
+            return this;
+        }
+
+        public GameObjectVisualBuilder Sprite(Sprite sprite)
+        {
+            var component = _gameObject.GetOrAddComponent<SpriteRenderer>();
+            component.sprite = sprite;
+            return this;
+        }
+
+        public GameObjectVisualBuilder SpriteColor(Color color)
+        {
+            var component = _gameObject.GetOrAddComponent<SpriteRenderer>();
+            component.color = color;
+            return this;
+        }
+
+        public GameObjectVisualBuilder LayerSortingOrder(int order)
+        {
+            var component = _gameObject.GetOrAddComponent<SpriteRenderer>();
+            component.sortingOrder = order;
+            return this;
+        }
+
+        public GameObjectVisualBuilder Scale(float x, float y)
+        {
+            var component = _gameObject.GetOrAddComponent<Transform>();
+            component.localScale = new Vector3(x, y);
+            return this;
+        }
+
+        #endregion
+    }
+}
