b8d3577f03b303610f98961e7267fbde5ded0708
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-03-21 12:01:30 +0500

    Base of dictionary serializator completed

---
+++ Assets/Homework5/DictionaryProxy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Homework5
+{
+    public sealed class DictionaryProxy<T1, T2>
+    {
+        public Dictionary<T1, T2> Dictionary;
+        public DictionaryProxy(Dictionary<T1, T2> dict)
+        {
+            Dictionary = dict;
+        }
+    }
+}
--- Assets/Homework5/LegacyDictionaryTypeSurrogate.cs
+++ Assets/Homework5/LegacyDictionaryTypeSurrogate.cs
@@ -1,19 +1,23 @@
-﻿using System;
-using System.CodeDom;
-using System.Collections.ObjectModel;
-using System.Reflection;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
+using System.Reflection;
+using System.CodeDom;
+using System;
 
 
 namespace Assets.Homework5
 {
-    class LegacyDictionaryTypeSurrogate : IDataContractSurrogate
+    public class LegacyDictionaryTypeSurrogate : IDataContractSurrogate
     {
         #region Methods
 
         public object GetCustomDataToExport(MemberInfo memberInfo, Type dataContractType)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("GetCustomDataToExport(Member)");
+            FieldInfo fieldInfo = (FieldInfo)memberInfo;
+            if (fieldInfo.IsPublic) return "public";
+            else return "private";
         }
 
         public object GetCustomDataToExport(Type clrType, Type dataContractType)
@@ -26,9 +30,33 @@ namespace Assets.Homework5
             throw new NotImplementedException();
         }
 
+        public Type GetDataContractType<T1, T2>(Type type)
+        {
+            Console.WriteLine("GetDataContractType");
+            if (typeof(Dictionary<T1, T2>).IsAssignableFrom(type))
+            {
+                return typeof(DictionarySurrogated<T1, T2>);
+            }
+            return type;
+        }
+
         public object GetDeserializedObject(object obj, Type targetType)
         {
             throw new NotImplementedException();
+        }
+
+        public object GetDeserializedObject<T1, T2>(object obj, Type targetType)
+        {
+            Console.WriteLine("GetDeserializedObject");
+            if (obj is DictionarySurrogated<T1, T2>)
+            {
+                Dictionary<T1, T2> dict = new Dictionary<T1, T2>();
+                var dictProxy = new DictionaryProxy<T1, T2>(dict);
+                dictProxy.Dictionary = ((DictionarySurrogated<T1, T2>)obj).dictionary;
+                
+                return dict;
+            }
+            return obj;
         }
 
         public void GetKnownCustomDataTypes(Collection<Type> customDataTypes)
@@ -39,6 +67,19 @@ namespace Assets.Homework5
         public object GetObjectToSerialize(object obj, Type targetType)
         {
             throw new NotImplementedException();
+        }
+
+        public object GetObjectToSerialize<T1, T2>(object obj, Type targetType)
+        {
+            Console.WriteLine("GetObjectToSerialize");
+            if (obj is Dictionary<T1, T2>)
+            {
+                var dictProxy = new DictionaryProxy<T1, T2>((Dictionary<T1,T2>)obj);
+                DictionarySurrogated<T1,T2> dictsur = new DictionarySurrogated<T1, T2>();
+                dictsur.dictionary = dictProxy.Dictionary;
+                return dictsur;
+            }
+            return obj;
         }
 
         public Type GetReferencedTypeOnImport(string typeName, string typeNamespace, object customData)
