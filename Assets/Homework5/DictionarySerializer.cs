2001fe481980baf82dbb5c26f6d5adadf33b5252
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-03-21 19:05:55 +0500

    Base commit

---
+++ Assets/Homework5/DictionarySerializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+namespace Assets.Homework5
+{
+    public class DictionarySerializer : MonoBehaviour
+    {
+        public Dictionary<int, string> Dictionary;
+    }
+}
