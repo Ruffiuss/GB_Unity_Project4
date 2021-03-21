971b1cdfa4a95c80342817475ed7df9e3ec679ae
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-03-21 19:00:12 +0500

    Clear progress, starting rework

--- Assets/Homework5/DictionarySerializeLoader.cs
+++ Assets/Homework5/DictionarySerializeLoader.cs
@@ -1,20 +1,10 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using System;
+﻿using UnityEngine;
 
 
 namespace Assets.Homework5
 {
-    [Serializable]
-    public class DictionarySerializeLoader : MonoBehaviour
+    public sealed class DictionartSerializeLoader : MonoBehaviour
     {
-        public Dictionary<int, float> example = new Dictionary<int, float>() { { 1, 2.0f }, { 22, 11.0f } };
-        public DictionarySurrogated<int, float> Dictionary;
-        public List<TKeySerializable<int, float>> Keys;
 
-        private void Awake()
-        {
-            var serializer = new DictionaryContractSerializerExample<int, float>(example);
-        }
     }
 }
