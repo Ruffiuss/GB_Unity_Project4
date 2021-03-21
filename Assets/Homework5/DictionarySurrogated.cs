357bc3d110534f8ffd2e8920a727b123330b1ef6
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-03-21 11:14:20 +0500

    Started Dictionary serializator Updated API Compatibility Level to .NET Framework 4.x

---
+++ Assets/Homework5/DictionarySurrogated.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+
+namespace Assets.Homework5
+{
+    [DataContract(Name = "Dictionary")]
+    public sealed class DictionarySurrogated<T1, T2>
+    {
+        [DataMember]
+        public Dictionary<T1, T2> dictionary;
+    }
+}
---
+++ Assets/Homework5/LegacyDictionaryTypeSurrogate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+
+namespace Assets.Homework5
+{
+    class LegacyDictionaryTypeSurrogate : IDataContractSurrogate
+    {
+        #region Methods
+
+        public object GetCustomDataToExport(MemberInfo memberInfo, Type dataContractType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetCustomDataToExport(Type clrType, Type dataContractType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type GetDataContractType(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetDeserializedObject(object obj, Type targetType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetKnownCustomDataTypes(Collection<Type> customDataTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetObjectToSerialize(object obj, Type targetType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type GetReferencedTypeOnImport(string typeName, string typeNamespace, object customData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CodeTypeDeclaration ProcessImportedType(CodeTypeDeclaration typeDeclaration, CodeCompileUnit compileUnit)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
