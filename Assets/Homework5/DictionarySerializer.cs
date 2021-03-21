dd385df5953c59e7e4da74e1049d6a3385405575
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-03-21 20:17:07 +0500

    Editor class created

--- Assets/Homework5/DictionarySerializer.cs
+++ Assets/Homework5/DictionarySerializer.cs
@@ -1,13 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using UnityEditor;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 
 namespace Assets.Homework5
 {
-    public class DictionarySerializer : MonoBehaviour
+    [Serializable]
+    [CustomEditor(typeof(DictionaryTest))]
+    public class DictionarySerializer : Editor
     {
-        public Dictionary<int, string> Dictionary;
+        #region Fields
+
+        private string[] _toolbarItems = new string[] { "Add Pair" };
+
+        #endregion
+
+
+        #region UnityMethods
+
+        public override void OnInspectorGUI()
+        {
+            DictionaryTest myTarget = (DictionaryTest)target;
+
+            GUILayout.Toolbar(0, _toolbarItems);
+
+            if (myTarget.Dictionary.Count.Equals(0))
+            {
+                DefineDictionary(ref myTarget.Dictionary);
+            }
+
+            int keyParsed;
+            string valueParsed;
+
+            foreach (var key in myTarget.Dictionary.Keys)
+            {
+                keyParsed = key;
+                EditorGUILayout.IntSlider(keyParsed, 0, 100);
+                valueParsed = myTarget.Dictionary[key];
+                EditorGUILayout.TextField("Value", valueParsed);
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private void DefineDictionary(ref Dictionary<int, string> dicitonary, int key = 0, string value = "none")
+        {
+            dicitonary = new Dictionary<int, string>() { { key, value } };
+        }
+
+        #endregion
     }
 }
---
+++ Assets/Homework5/DictionaryTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Homework5
+{    
+    public class DictionaryTest : MonoBehaviour
+    {
+        public Dictionary<int, string> Dictionary = new Dictionary<int, string>();
+    }
+}
