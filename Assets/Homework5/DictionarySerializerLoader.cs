9ec841e857d238815c5b9ac021e704d9e8809a0f
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-03-21 12:45:07 +0500

    Dictionary xml serialization completed

---
+++ Assets/Homework5/DictionarySerializerLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Homework5
+{
+    public class DictionarySerializerLoader : MonoBehaviour
+    {
+        public Dictionary<int, float> example = new Dictionary<int, float>() { { 1, 2.0f } };
+
+        private void Awake()
+        {
+            var serializer = new DictionaryContractSerializerExample<int, float>(example);
+        }
+    }
+}
